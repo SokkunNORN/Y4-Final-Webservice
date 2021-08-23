03c9a94ddc3b958dcb70165de4a4f7cf3c246f4a
sokkun <sokkun.norn2019@gmail.com>
2021-08-23 18:23:45 +0700

    update get data from db

--- db/connection.php
+++ db/connection.php
@@ -5,14 +5,9 @@ $UNAME = 'root';
 $PASS = '';
 $DB_NAME = 'computer_shop';
 
-$conn = mysqli_connect($S_NAME, $UNAME, $PASS, $DB_NAME);
+$conn = new mysqli($S_NAME, $UNAME, $PASS, $DB_NAME);
     
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
+if (!$conn) {
+    die(mysqli_error($conn));
 }
 
-// define('DB_USER', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_DATABASE', 'computer_shop');
-// define('DB_SERVER', 'localhost');
