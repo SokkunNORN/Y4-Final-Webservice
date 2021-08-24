be7bf491e23ee280824e380c79dd3b944158695d
sokkun <sokkun.norn2019@gmail.com>
2021-08-24 13:56:00 +0700

    add stripe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0f3df6a538f85f0a5b3ca6f3d249da5390a142a6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0f3df6a538f85f0a5b3ca6f3d249da5390a142a6',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.94.0',
+            'version' => '7.94.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '03cd6b5c1c4fc9087ddd1dd046457bff274294d6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
