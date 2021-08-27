3e09d318b3fba75bff2c42ba23862584323058d6
sokkun <sokkun.norn2019@gmail.com>
2021-08-27 17:13:22 +0700

    update query

--- view/checkout.php
+++ view/checkout.php
@@ -1,12 +1,13 @@
 <?php 
     require('./db/connection.php');
 
-    $query = "SELECT product.*, product_process.*, SUM(product_process.number_product) as 'p_total'
-    FROM `product_process`,`product` 
-    WHERE product.id = product_process.pid 
-    AND product_process.buyer_id = 1
-    AND product_process.seller_id = 2
-    GROUP BY product_process.pid;";
+    $query = "SELECT product.*, product_process.*, 
+            SUM(product_process.number_product) as 'p_total'
+            FROM `product_process`,`product`
+            WHERE product.id = product_process.pid
+            AND product_process.buyer_id = 1
+            AND product_process.seller_id = 2
+            GROUP BY product_process.pid;";
 
     $results = $conn->query($query);
     if ($results) {
