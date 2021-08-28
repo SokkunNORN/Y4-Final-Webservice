30c84443ba0c15fd8c52b23f6dbdd2e49a5ea74c
sokkun <sokkun.norn2019@gmail.com>
2021-08-28 14:55:04 +0700

    add product history

--- view/checkout.php
+++ view/checkout.php
@@ -3,13 +3,16 @@
 
 	$buyer_id = $_SESSION['id'];
 
+	if (isset($_SESSION['id'])) {
+
     $query = "SELECT product.*, product.id as product_id, 
 			product_process.*, 
             SUM(product_process.number_product) as 'p_total'
             FROM `product_process`,`product`
             WHERE product.id = product_process.pid
+			AND product_process.status_id = 2
             AND product_process.buyer_id = $buyer_id
-            GROUP BY product_process.pid;";
+            GROUP BY product_process.pid";
 
     $results = $conn->query($query);
     if ($results) {
@@ -51,87 +54,77 @@
         </tbody>
     </table>
 	  <form method="post" action="#">
-      <div class="modal-content">
-		  
-        <div class="modal-body">
-          	<h4 class="modal-title">Customer Detail</h4>
-			<br>
-			<br>
-		  	<div class="form-group">
-				<input type="text" class="form-control form-control-sm" placeholder="Card holder name" required>
-			</div>
-				<br>
-		  	<div class="form-group">
-				<input type="email" class="form-control form-control-sm" placeholder="Email" required>
-			</div>
-				<br>
-		  	<div class="form-group">
-				<textarea type="text" class="form-control form-control-sm" placeholder="Address"></textarea>
-			</div>
-				<br>
-			<div class="row">
-				<div class="col-6">
-					<div class="form-group">
-						<input type="text" class="form-control form-control-sm" placeholder="City" required>
-					</div>
-				</div>
-				<br>
-				<div class="col-6">
-					<div class="form-group">
-						<input type="number" class="form-control form-control-sm" placeholder="Zip" required>
-					</div>
-				</div>
-			</div>
-			<br>
-			<div class="row">
-				<div class="col-6">
-					<div class="form-group">
-						<input type="text" class="form-control form-control-sm" placeholder="State" required>
-					</div>
-				</div>
-				<div class="col-6">
-					<div class="form-group">
-						<input type="text" class="form-control form-control-sm" placeholder="Country" required>
-					</div>
-				</div>
-			</div>
-			<hr>
-			<h6 class='text-center'>Payment Detail</h6>
-			<div class="form-group">
-				<input type="number" class="form-control form-control-sm" placeholder="Card Number" required>
-			</div>
-			<br>
-
-			<div class="row">
-				<div class="col-4">
-					<div class="form-group">
-						<input type="number" class="form-control form-control-sm" placeholder="Expiry Month" required>
-					</div>
-				</div>
-				<div class="col-4">
-					<div class="form-group">
-						<input type="number" class="form-control form-control-sm" placeholder="Expiry Year" required>
-					</div>
-				</div>
-				<div class="col-4">
-					<div class="form-group">
-						<input type="number" class="form-control form-control-sm" placeholder="CVC" required>
-					</div>
-				</div>
-			</div>
-        </div>
-        
-        <div class="modal-footer" style="border: none;">
-          <button type="submit" class="btn btn-primary btn-sm" name="btn_paynow">Pay Now</button>
-        </div>
-        
-      </div>
+		<div class="modal-footer" style="border: none;">
+			<button type="submit" class="btn btn-outline-danger btn-sm" name="btn_clear">Clear</button>
+			<button type="submit" class="btn btn-primary btn-sm" name="btn_paynow">Pay Now</button>
+		</div>
 	  </form>
     </div>
 </div>
 </div>
 <?php
-    } else {
-		echo '<h2 class="mt-4 text-center">Please login...</h2>';
+    }
+
+	$query = "SELECT product.*, product.id as product_id, 
+			product_process.*, 
+			SUM(product_process.number_product) as 'p_total'
+			FROM `product_process`,`product`
+			WHERE product.id = product_process.pid
+			AND product_process.status_id != 2
+			AND product_process.buyer_id = $buyer_id
+			GROUP BY product_process.pid";
+
+    $results_history = $conn->query($query);
+	if ($results_history) {
+?>
+<div class="container mt-4">
+	<div class="row">
+		<div class="col-3"></div>
+
+		<div class="col-6">
+			<hr>
+			<h4>Product History</h4>
+			<table class="table table-lg mt-4 table-striped">
+				<thead>
+					<tr>
+						<th>Product Name</th>
+						<th>Quantity</th>
+						<th>Price</th>
+						<th>Total</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						while ($row = mysqli_fetch_assoc($results_history)) {
+					?>
+					<tr>
+						<td><?php echo $row['name']; ?></td>
+						<td><?php echo $row['p_total']; ?></td>
+						<td>$ <?php echo $row['price']; ?></td>
+						<td>$ <?php echo $row['price'] * $row['p_total']; ?></td>
+						<td>
+							<?php 
+								if ($row['status_id'] == 3) {
+									echo 'Ordering';
+								} else if ($row['status_id'] == 4) {
+									echo 'Sold';
+								}
+							?>
+						</td>
+					</tr>
+					<?php
+						}
+					?>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
+</div>
+<?php
 	}
+} else {
+	echo '<h2 class="mt-4 text-center">Please login...</h2>';
+}
 ?>
