0f0ca51a8758b10ad7fa6ed11c89d799ad3dfe3a
Sovannthai <n-sovannthai@beniten.com>
2021-08-30 22:54:19 +0700

    update stripe

--- process/product_process.php
+++ process/product_process.php
@@ -43,7 +43,7 @@ if (isset($_POST['btn-add-product'])) {
 
     $query = "INSERT INTO `product` (name, 	description, image, price, discount, brand_id, uid)
             VALUES ('$pname', '$pdescription', 'asus.jpeg', $pprice, $pdiscount, $pbrand, $uid)";
-    
+
     $conn->query($query);
 
     header("Location: ../?link=home");
@@ -54,7 +54,7 @@ if (isset($_POST['remove'])) {
     $id = $_POST['product_id'];
 
     $query = "DELETE FROM `product` WHERE id = $id";
-    
+
     $conn->query($query);
 
     header("Location: ../?link=home");
@@ -75,17 +75,26 @@ if (isset($_POST['btn_clear_checkout'])) {
 if (isset($_POST['token'])) {
     $buyer_id = $_SESSION['id'];
 
-    $query = "UPDATE `product_process` 
-            SET `status_id` = 3
-            WHERE buyer_id = $buyer_id
-            AND status_id = 2";
+    $query = "SELECT product.*, product.id as product_id, 
+        product_process.*, 
+        SUM(product_process.number_product) as 'p_total'
+        FROM `product_process`,`product`
+        WHERE product.id = product_process.pid
+        AND product_process.status_id = 2
+        AND product_process.buyer_id = $buyer_id
+        GROUP BY product_process.pid";
 
-    $conn->query($query);
-
+    $results = $conn->query($query);
+    $name= null;
+    $total_amount=0;
+    while ($row = mysqli_fetch_assoc($results)) {
+        $name .= $row['name'] .', ';
+        $total_amount += $row['price'] * $row['p_total'];
+    }
     require_once '../vendor/autoload.php';
- 
+
     \Stripe\Stripe::setApiKey('sk_test_51Hc3WOFDPuwTsMlm6FqTwLBqy5wMoKpLKkbKOEaNgMDN1X4QozIjJgZQbNL1IvcaMNUVDQR5i10BruwROUTr1HNK00cOXuij72');
- 
+
     $customer = \Stripe\Customer::create(array(
         'email'   => $_SESSION['email'],
         'source'  => $_POST["token"],
@@ -99,52 +108,26 @@ if (isset($_POST['token'])) {
         )
     ));
     $order_number = rand(100000, 999999);
- 
+
     $charge = \Stripe\Charge::create(array(
         'customer'  => $customer->id,
-        'amount'  => 1000 * 100,
+        'amount'  => $total_amount *100,
         'currency'  => "USD",
-        'description' => "it say about compoter shop",
+        'description' => $name,
         'metadata'  => array(
             'order_id'  => $order_number
         )
     ));
- 
+
     $response = $charge->jsonSerialize();
- 
-    // if ($response["amount_refunded"] == 0 && empty($response["failure_code"]) && $response['paid'] == 1 && $response["captured"] == 1 && $response['status'] == 'succeeded') {
-    //     $amount = $response["amount"] / 100;
- 
-    //     $order_data = array(
-    //         'order_number'   => $order_number,
-    //         'order_total_amount' => $amount,
-    //         'transaction_id'  => $response["balance_transaction"],
-            // $_SESSION['id'] 
-            // $_SESSION['uname']  
-            // $_SESSION['email']  
-            // $_SESSION['address'] 
-            // $_SESSION['city']  
-            // $_SESSION['zip']  
-            // $_SESSION['state'] 
-            // $_SESSION['country']  
-            // $_SESSION['cardNumber']  
-            // $_SESSION['expiryMonth']  
-            // $_SESSION['expiryYear']  
-            // $_SESSION['cvc'] 
-            // $_SESSION['role_id'] 
-        // );
- 
-        //     $query = "
-        //         INSERT INTO user 
-        //             (order_number, order_total_amount, transaction_id, card_cvc, card_expiry_month, card_expiry_year, order_status, card_holder_number, email_address, customer_name, customer_address, customer_city, customer_pin, customer_state, customer_country) 
-        //             VALUES (:order_number, :order_total_amount, :transaction_id, :card_cvc, :card_expiry_month, :card_expiry_year, :order_status, :card_holder_number, :email_address, :customer_name, :customer_address, :customer_city, :customer_pin, :customer_state, :customer_country)
-        //         ";
-    
-        //     $statement = $connect->prepare($query);
-            
-        //     $statement->execute($order_data);
 
+    if ($response["amount_refunded"] == 0 && empty($response["failure_code"]) && $response['paid'] == 1 && $response["captured"] == 1 && $response['status'] == 'succeeded') {
+        $query = "UPDATE `product_process` 
+            SET `status_id` = 3
+            WHERE buyer_id = $buyer_id
+            AND status_id = 2";
 
-        header("Location: ../?link=checkout");
-    // }
+        $conn->query($query);
+    }
+    header("Location: ../?link=checkout");
 }
