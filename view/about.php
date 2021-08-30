d78a84423cd617955c386d38c417593b6709caf9
sokkun <sokkun.norn2019@gmail.com>
2021-08-30 12:01:44 +0700

    add page about

---
+++ view/about.php
@@ -0,0 +1,38 @@
+
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">About Us</h1>
+        <p class="lead text-muted">We're from group 8 from the University of Puthisastra in class ORM Webservice. We created a website for selling and buying computer products from all collection brands and shops. We want to improve online marketing in our country.</p>
+      </div>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container text-center">
+      <h2 class="fw-light">Our Team</h2>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <div class="col-lg-6 p-4">
+            <img src="./asset/author/sokkun.jpeg" class="rounded-circle" alt="sokkun" width="140" height="140">
+            <br>
+            <br>
+            <h2 >Norn Sokkun</h2>
+            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+        </div>
+        
+        <div class="col-lg-6 p-4">
+            <img src="./asset/author/sovanthai.jpeg" class="rounded-circle" alt="sovanthai" width="140" height="140">
+            <br>
+            <br>
+            <h2>Neang Sovanthai</h2>
+            <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</main>
